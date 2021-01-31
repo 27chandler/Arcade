cb22238951f4e3989a54e13a1e59972c6fd26ec6
27chandler <chandler27s@yahoo.com>
2021-01-31 22:16:19 +0000

    Fixed Pong Prefab

--- SuperNova/SuperNova/Assets/Assets/Mechanics/Pong/PongControls.cs
+++ SuperNova/SuperNova/Assets/Assets/Mechanics/Pong/PongControls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PongControls : PlayerInput, IControllable
 {
     [SerializeField] private bool _areControlsLocked;
+    [SerializeField] private float _borderTop, _borderBottom;
 
     void Start()
     {
@@ -22,6 +23,15 @@ public class PongControls : PlayerInput, IControllable
         {
             Vector3 movement = new Vector3();
             movement.y = movement_direction.y;
+
+            if ((movement.y > 0.0f) && (transform.localPosition.y >= _borderTop))
+            {
+                return;
+            }
+            if ((movement.y < 0.0f) && (transform.localPosition.y <= _borderBottom))
+            {
+                return;
+            }
 
             transform.localPosition += movement * Time.deltaTime;
         }
