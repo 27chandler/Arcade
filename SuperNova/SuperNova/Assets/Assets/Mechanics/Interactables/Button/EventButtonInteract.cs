7aaecd0e8aa22cb88da2d67b195cc69bbff414b4
27chandler <chandler27s@yahoo.com>
2021-03-18 00:46:21 +0000

    Added event buttons

---
+++ SuperNova/SuperNova/Assets/Assets/Mechanics/Interactables/Button/EventButtonInteract.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventButtonInteract : Interaction
+{
+    [SerializeField] private UnityEvent _activationEvents;
+
+    public override void ActivateInteraction(PlayerInteract player_interactor)
+    {
+        _activationEvents.Invoke();
+    }
+}
