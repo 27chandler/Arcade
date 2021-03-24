8accae66503cf73bf5ba22a455d70b6bfcc4f151
27chandler <chandler27s@yahoo.com>
2021-03-24 16:17:49 +0000

    Added states to event buttons

--- SuperNova/SuperNova/Assets/Assets/Mechanics/Interactables/Button/EventButtonInteract.cs
+++ SuperNova/SuperNova/Assets/Assets/Mechanics/Interactables/Button/EventButtonInteract.cs
@@ -5,10 +5,17 @@ using UnityEngine.Events;
 
 public class EventButtonInteract : Interaction
 {
-    [SerializeField] private UnityEvent _activationEvents;
+    [SerializeField] private int _state = 0;
+    [SerializeField] private List<UnityEvent> _activationEvents;
 
     public override void ActivateInteraction(PlayerInteract player_interactor)
     {
-        _activationEvents.Invoke();
+        _state++;
+        if (_state >= _activationEvents.Count)
+        {
+            _state = 0;
+        }
+
+        _activationEvents[_state].Invoke();
     }
 }
